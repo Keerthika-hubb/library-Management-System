4e590a0b5e5d2053b9228d289cb327fcac7b2bca
Keerthika S <keerthika.2832004@gmail.com>
2026-07-21 16:27:14 +0530

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -3,5 +3,35 @@ public class Book {
     int bookId;
     String title;
     String author;
+    public Book(int bookId, String title, String author) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+    }
 
+    public int getBookId() {
+        return bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
+
+
