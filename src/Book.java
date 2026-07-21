ae5c856338f27a4f6b6d16ec6fe2b2b8b19cb55b
Keerthika S <keerthika.2832004@gmail.com>
2026-07-22 00:42:12 +0530

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,37 +1,9 @@
 public class Book {
+    private int bookId;
+    private String title;
+    private String author;
+    private boolean isAvailable;
 
-    int bookId;
-    String title;
-    String author;
-    public Book(int bookId, String title, String author) {
-        this.bookId = bookId;
-        this.title = title;
-        this.author = author;
-    }
-
-    public int getBookId() {
-        return bookId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setBookId(int bookId) {
-        this.bookId = bookId;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
 }
-
-
+    
+    
