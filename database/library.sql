7671113602ee1f9f5514bb10c0a60aca375042f1
Keerthika S <keerthika.2832004@gmail.com>
2026-07-22 12:58:55 +0530

    Create library_Added library database schema and sample book recordsmanagement database and Books table
    This SQL script creates a database for managing a library, including a table for books with relevant fields and initial data.

--- database/library.sql
+++ database/library.sql
@@ -1,1 +1,21 @@
+CREATE DATABASE library_management;
 
+USE library_management;
+
+CREATE TABLE Books (
+      Book_ID INT PRIMARY KEY AUTO_INCREMENT,
+      Title VARCHAR(100) NOT NULL,
+      Author VARCHAR(100) NOT NULL,
+      Category VARCHAR(50),
+      Status VARCHAR(20) DEFAULT 'Available',
+      Issued_Date DATE,
+      Return_Date DATE
+      );
+INSERT INTO Books (Title, Author, Category)
+VALUES
+('Harry Potter', 'J.K. Rowling', 'Fantasy'),
+('Atomic Habits', 'James Clear', 'Self Help'),
+('The Alchemist', 'Paulo Coelho', 'Fiction'),
+('Wings of Fire', 'A.P.J. Abdul Kalam', 'Biography');
+
+SELECT * FROM Books;
