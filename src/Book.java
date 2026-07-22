dde2334dacfe7633eb9725fddd7514e34e11eedc
Keerthika S <keerthika.2832004@gmail.com>
2026-07-22 13:34:02 +0530

    Added parameterized constructor to Book class

--- src/Book.java
+++ src/Book.java
@@ -5,5 +5,13 @@ public class Book {
     private String author;
     private String category;
     private String status;
+    public Book(int bookId, String title, String author, String category, String status) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.category = category;
+        this.status = status;
+
+    }
 
 }
