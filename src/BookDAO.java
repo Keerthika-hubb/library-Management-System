6efe889adac927d6e4e43e800675e3597341cc7b
Keerthika S <keerthika.2832004@gmail.com>
2026-07-22 14:40:16 +0530

    Update BookDAO.java

--- src/BookDAO.java
+++ src/BookDAO.java
@@ -1,18 +1,1 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-public class DatabaseConnection {
-  private static final String URL = "jdbc:mysql://localhost:3306/library_management";
-  private static final String USER = "root";
-  private static final String PASSWORD = "YOUR_PASSWORD";
 
-  public static Connection getConnection() {
-    try {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
-    }
-    catch (SQLException e) {
-           e.printStackTrace();
-           return null;
-    }
-  }
-} 
