69208cd46473c1fe7075c419c8606f109f18c322
Keerthika S <keerthika.2832004@gmail.com>
2026-07-22 14:42:47 +0530

    Implement addBook method in BookDAO
    Added method to initiate book addition in BookDAO.

--- src/BookDAO.java
+++ src/BookDAO.java
@@ -1,1 +1,12 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+public class BookDAO {
+
+    public void addBook(Book book) {
+
+        Connection connection = DatabaseConnection.getConnection();
+
+    }
+}
